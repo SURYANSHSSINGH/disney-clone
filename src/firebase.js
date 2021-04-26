260654d6c592768eb470660a7aa7b33b14622bfb
Suryansh Singh <46718550+suryansh1168@users.noreply.github.com>
2021-04-27 00:59:13 +0530

    Initial Commit
    Disney-plus clone first commit | Contains Navbar,signin and signup functionality

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCG8j5pM9sXPXiztn_GRaiHw9QVerD0Fsg",
+    authDomain: "disney-clone-8986e.firebaseapp.com",
+    projectId: "disney-clone-8986e",
+    storageBucket: "disney-clone-8986e.appspot.com",
+    messagingSenderId: "410107210233",
+    appId: "1:410107210233:web:27139febdc85519434f528",
+    measurementId: "G-TCYDHC7RN9"
+  };
+
+  const firebaseapp=firebase.initializeApp(firebaseConfig);
+  const db= firebaseApp.firestore();
+  const auth= firebase.auth();
+  const provider= new firebase.auth.GoogleAuthProvider();
+  const storage= firebase.storage();
+
+  export{ auth, provider,storage};
+  export default db;
+  
