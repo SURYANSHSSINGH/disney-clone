a690218acec825d8cba930f1068a642f59d3ba62
Suryansh Singh <46718550+suryansh1168@users.noreply.github.com>
2021-05-03 00:49:29 +0530

    Firebase changes

--- src/Comp/Header.js
+++ src/Comp/Header.js
@@ -3,10 +3,14 @@ import styled from "styled-components";
 import { auth, provider } from "../firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { selectUserName, selectUserPhoto, setUserLoginDetails, setSignOutState } from "../features/user/userSlice";
+import {
+  selectUserName,
+  selectUserPhoto,
+  setUserLoginDetails,
+  setSignOutState,
+} from "../features/user/userSlice";
 
 const Header = (props) => {
-
   const dispatch = useDispatch();
   const history = useHistory();
   const userName = useSelector(selectUserName);
@@ -23,14 +27,16 @@ const Header = (props) => {
 
   const handleAuth = () => {
     if (!userName) {
-      auth.signInWithPopup(provider).then((result) => {
-        setUser(result.user);
-        console.log(result);
-      }).catch((error) => {
-        alert(error.message);
-      });
-    }
-    else if (userName) {
+      auth
+        .signInWithPopup(provider)
+        .then((result) => {
+          setUser(result.user);
+          console.log(result);
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+    } else if (userName) {
       auth
         .signOut()
         .then(() => {
@@ -39,7 +45,6 @@ const Header = (props) => {
         })
         .catch((err) => alert(err.message));
     }
-
   };
 
   const setUser = (user) => {
@@ -56,50 +61,50 @@ const Header = (props) => {
       <Logo>
         <img src="/images/logo.svg" alt="" />
       </Logo>
-      {
-        !userName ? (<Signin onClick={handleAuth}>Signin</Signin>)
-          : (
-            <>
-              <Menu>
-                <a href="/home">
-                  <img src='/images/home-icon.svg' alt="HOME" />
-                  <span>HOME</span>
-                </a>
-                <a>
-                  <img src='/images/search-icon.svg' alt="SEARCH" />
-                  <span>SEARCH</span>
-                </a>
-                <a>
-                  <img src='/images/watchlist-icon.svg' alt="WATCHLIST" />
-                  <span>WATCHLIST</span>
-                </a>
-                <a>
-                  <img src='/images/original-icon.svg' alt="ORIGINALS" />
-                  <span>ORIGINALS</span>
-                </a>
-                <a>
-                  <img src='/images/movie-icon.svg' alt="MOVIES" />
-                  <span>MOVIES</span>
-                </a>
-                <a>
-                  <img src='/images/series-icon.svg' alt="SERIES" />
-                  <span>SERIES</span>
-                </a>
-              </Menu>
-              <SignOut>
-                <UserImg src={userPhoto} alt={userName} />
-                <DropDown>
-                  <span onClick={handleAuth}>Sign out</span>
-                </DropDown>
-              </SignOut>
-            </>
-          )}
+      {!userName ? (
+        <Signin onClick={handleAuth}>Signin</Signin>
+      ) : (
+        <>
+          <Menu>
+            <a href="/home">
+              <img src="/images/home-icon.svg" alt="HOME" />
+              <span>HOME</span>
+            </a>
+            <a>
+              <img src="/images/search-icon.svg" alt="SEARCH" />
+              <span>SEARCH</span>
+            </a>
+            <a>
+              <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+              <span>WATCHLIST</span>
+            </a>
+            <a>
+              <img src="/images/original-icon.svg" alt="ORIGINALS" />
+              <span>ORIGINALS</span>
+            </a>
+            <a>
+              <img src="/images/movie-icon.svg" alt="MOVIES" />
+              <span>MOVIES</span>
+            </a>
+            <a>
+              <img src="/images/series-icon.svg" alt="SERIES" />
+              <span>SERIES</span>
+            </a>
+          </Menu>
+          <SignOut>
+            <UserImg src={userPhoto} alt={userName} />
+            <DropDown>
+              <span onClick={handleAuth}>Sign out</span>
+            </DropDown>
+          </SignOut>
+        </>
+      )}
     </Nav>
   );
 };
 
 const Nav = styled.nav`
-position: fixed;
+  position: fixed;
   top: 0;
   left: 0;
   right: 0;
@@ -143,6 +148,7 @@ a{
     align-items: center;
     padding:0 25px;
 
+
 img{
     height:20px;
     min-width:20px;
@@ -157,6 +163,8 @@ span{
     padding:2px 0px;
     white-space: nowrap;
     position:relative;
+    
+
 
 &:before{
     content:"";
@@ -173,9 +181,9 @@ span{
     transition: all 250ms cubic-bezier(0.25, 0.46, 0.4,0.3);
     visibility:hidden;
     width:auto;
+    }  
+  }
 
-    }
-}
 &:hover {
     span:before{
     transform:scaleX(1);
@@ -187,23 +195,25 @@ span{
         }
 
     }
-}
+     @media (max-width: 768px) {
+      display: none;
+    } 
+
 `;
 
 const Signin = styled.a`
-background-color: rgba(0, 0, 0, 0.6);
-padding: 8px 16px;
-text-transform: uppercase;
-letter-spacing: 1.5px;
-border: 1px solid #f9f9f9;
-border-radius:6px;
-transition: all .2s ease 0s;
-
-&:hover {
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+  &:hover {
     background-color: #f9f9f9;
-    colour: #000000;
+    color: #000;
     border-color: transparent;
-}
+  }
 `;
 
 const UserImg = styled.img`
@@ -247,6 +257,5 @@ const SignOut = styled.div`
     }
   }
 `;
-
 
 export default Header;
